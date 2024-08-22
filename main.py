1ad15ea9fac747e25bb7f60e258c957dab5ce129
Jean Kochenborger <35958882-jeanhk2014@users.noreply.replit.com>
2024-08-22 14:26:52 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import random  # Import statement should be at the top of the file
+
+
+def get_number():
+  return random.randint(1, 6)
+  
+a = get_number()
+b = get_number()
+
+if (a + b) == 12:
+  print("You rolled a double six!")
+elif a == b:
+  print("You rolled a double in your dice!.")
+  print(f"You rolled {a} and {b}")
+else:
+  print(f"You rolled {a} and {b}")
